ec52c693a3e611dc60056f5b5966c4c64dcf6569
thaihoangcfc <thaihoangcfc@gmail.com>
2017-03-10 14:16:39 +1100

    Change text for the score so it draws a more interesting font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -13,6 +13,7 @@ namespace CardGames
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
             SwinGame.LoadSoundEffectNamed ("Slap", "slap.wav");
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
+        	SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
         }
 
 		/// <summary>
@@ -59,8 +60,8 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, "GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
